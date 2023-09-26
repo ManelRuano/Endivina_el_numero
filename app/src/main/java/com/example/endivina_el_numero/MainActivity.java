7cbce904c9a47ff375ff0ca5d7c257e6bcffafe9
ManelRuano <ruano.garcia.manel@gmail.com>
2023-09-26 17:32:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/endivina_el_numero/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.endivina_el_numero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
